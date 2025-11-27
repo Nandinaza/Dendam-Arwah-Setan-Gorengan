259e81f7f19cc84f0aac65acb18bbb88da29547c
Nandinaza <nandichanel@gmail.com>
2025-11-28 06:03:39 +0700

    complete-main-menu

---
+++ Script/Level_script/Prologue/ulang_tahun_pito.gd
@@ -0,0 +1,5 @@
+extends Node3D
+
+func _ready() -> void:
+	#load some sound settings
+	FadeTransition.play_fade_in("12 Agustus 2034", "Lokasi Rumah Pito")
--- Script/UI_Interfacec/main_menu.gd
+++ Script/UI_Interfacec/main_menu.gd
@@ -7,8 +7,11 @@ extends Control
 @onready var hover_sound = $Hover_sound
 @onready var background_music = $Background_Music
 
-@onready var full_screnn_btn = $Settings_menu/NinePatchRect/VBoxContainer/CheckBox
-@onready var android_mode_btn = $Settings_menu/NinePatchRect/VBoxContainer/CheckBox2
+@onready var gpu_particles_3d: GPUParticles3D = $SubViewportContainer/SubViewport/Node3D/GPUParticles3D
+
+#wiggle camera
+@onready var camera_viewport: Camera3D = $SubViewportContainer/SubViewport/Node3D/Camera3D
+var a = 0.0
 
 @export var setting_vr: settings
 
@@ -20,14 +23,14 @@ func _ready():
 	audio_stream_player.volume_db = setting_vr.sound_effect
 	hover_sound.volume_db = setting_vr.sound_effect
 	background_music.volume_db = setting_vr.background_music
-
-	setting_vr.sistem_operasi = OS.get_name()
-
-	match OS.get_name():
-		"Android": android_start_setting()
-		"Windows": dekstop_start_setting()
-		"Web": dekstop_start_setting()
 	
+	gpu_particles_3d.emitting = true
+
+func _process(delta: float) -> void:
+	a += delta
+	#-3.41 2.352 -13.952
+	camera_viewport.position.y = 2 + (sin(a) * 0.2)
+
 func setting_toggle():
 	audio_stream_player.play()
 	mainmenu.visible = not mainmenu.visible
@@ -36,7 +39,7 @@ func setting_toggle():
 #Button Pressed
 func _on_play_button_pressed():
 	audio_stream_player.play()
-	get_tree().change_scene_to_file("res://Main_story/level1dmo.tscn")
+	get_tree().change_scene_to_file("res://Main_story/Prologue_0/Ulang_tahun_pito.tscn")
 func _on_setting_button_pressed():
 	setting_toggle()
 func _on_exit_button_pressed():
@@ -49,13 +52,6 @@ func _on_exit_button_pressed():
 func _on_back_pressed():
 	setting_toggle()
 
-#checkbox
-func _on_check_box_pressed():
-	audio_stream_player.play()
-	if full_screnn_btn.button_pressed:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		
 func _on_check_box_2_pressed():
 	audio_stream_player.play()
@@ -90,13 +86,3 @@ func _on_sfx_value_changed(value):
 
 func _on_background_music_finished():
 	background_music.play()
-
-#mode
-func android_start_setting():
-	android_mode_btn.button_pressed = true
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	full_screnn_btn.toggle_mode = bool(1)
-	full_screnn_btn.disabled = true
-	
-func dekstop_start_setting():
-	print("web/windows mode")
