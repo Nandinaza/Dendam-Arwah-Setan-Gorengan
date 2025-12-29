c0f198317c3277a1c40e1ce7962b9e3d4ddd34c5
Nandinaza <nandichanel@gmail.com>
2025-12-29 22:22:21 +0700

    end2025

--- Template/intractable/Object/pick_able_item_tmp.gd
+++ Template/intractable/Object/pick_able_item_tmp.gd
@@ -4,6 +4,9 @@ func _ready() -> void:
 	print(intraction_promp)
 
 func _on_intract(player):
-	if player.has_method("add_to_inventory"):
-		player.add_to_inventory("key", 123)
-		queue_free()
+	Dialogic.start(
+		"res://dialogic/Rumah_pito_0/pito_ulang_tahun_0.dtl"
+	)
+	#if player.has_method("add_to_inventory"):
+		#player.add_to_inventory("key", 123)
+		#queue_free()
